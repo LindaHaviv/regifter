4679e79b58837c176bb8591e91ac8622bac014c1
LindaHaviv <linda.haviv@gmail.com>
2016-01-28 13:26:03 -0500

    Create migrations for User, Gift, Category, GiftCategories join table

---
+++ db/migrate/20160128175354_create_gifts.rb
@@ -0,0 +1,13 @@
+class CreateGifts < ActiveRecord::Migration
+  def change
+    create_table :gifts do |t|
+      t.string :title
+      t.integer :value
+      t.text :description
+      t.string :brand
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20160128182349_create_gift_categories.rb
@@ -0,0 +1,10 @@
+class CreateGiftCategories < ActiveRecord::Migration
+  def change
+    create_table :gift_categories do |t|
+      t.references :gift, index: true, foreign_key: true
+      t.references :category, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
