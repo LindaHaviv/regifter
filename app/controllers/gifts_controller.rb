2215ef28416d8f1c36133b98c168286a14d68383
nat-ser <nataliya.seryakova@gmail.com>
2016-01-29 12:38:47 -0500

    crud for gift

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -9,20 +9,38 @@ class GiftsController < ApplicationController
   end
 
   def create
-    binding.pry
     @gift = Gift.create(gift_params)
-
-
+    redirect_to @gift
   end
 
   def edit
+    set_gift
+  end
+
+  def update
+    set_gift
+    @gift.update(gift_params)
+    redirect_to @gift
   end
 
   def show
     @gift = Gift.find(params[:id])
   end
 
+  def destroy
+    set_gift
+    @gift.destroy
+
+    respond_to do |format|
+      format.html { redirect_to gifts_url, notice: 'Gift was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
+  def set_gift
+    @gift = Gift.find(params[:id])
+  end
 
   def gift_params
     params.require(:gift).permit(:title, :value, :description, :brand, :image, :user_id, :categories_attributes=>[:title], :category_ids=>[])
