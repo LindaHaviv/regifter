84cc2a496e2918595792f2e57491ae08dea13451
leighsn <leighscherrer@gmail.com>
2016-02-01 11:45:27 -0500

    Added validation on user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      binding.pry
       redirect_to @user,:notice => "Thank you for signing up to Regifter, #{@user.username}!"
     else
-      redirect_to signup_path
+      binding.pry
+      @errors = @user.errors
+      render "new"
     end
   end
 
