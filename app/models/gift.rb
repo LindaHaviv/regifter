c6c2b4376857ffdec1f827d414cd69dc0b3a2fa0
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2016-02-02 13:23:37 -0500

    pending swapssssss

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,10 +1,10 @@
 class Gift < ActiveRecord::Base
   belongs_to :user
-  has_many :requests
+  # has_many :requests
 
   # has_many :requests, foreign_key: :wanted_id
 
-  accepts_nested_attributes_for :requests
+  # accepts_nested_attributes_for :requests
   has_many :gift_categories, dependent: :destroy
   has_many :categories, through: :gift_categories, dependent: :destroy
   accepts_nested_attributes_for :categories
@@ -12,10 +12,12 @@ class Gift < ActiveRecord::Base
   # searchkick autocomplete: ['title', 'brand']
   mount_uploader :image, AssetUploader
 
-  # def requests_wanted
-  #   Request.where(wanted_id: self.id)
-  # end
-  # def requests_made
-  #   Request.where(unwanted_id: self.id)
-  # end
+  def requests_wanted
+    Request.where(wanted_id: self.id)
+  end
+
+  def requests_made
+    Request.where(unwanted_id: self.id)
+  end
+
 end
