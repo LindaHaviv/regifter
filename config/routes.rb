83ac445b761eab527ac1b3e72d07c660b266c2cf
LindaHaviv <linda.haviv@gmail.com>
2016-02-02 13:15:06 -0500

    made a home page for now so we can demo it in a pretty way

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'gift_swaps/index'
 
-  get 'gift_swaps/new'
-
-  get 'gift_swaps/create'
-
-  root "gifts#index"
+  root "home#index"
 
   delete 'signout', to: "sessions#destroy"
 
