45da649405d743fbcfa58a257bf66aa3650e59d1
leighsn <leighscherrer@gmail.com>
2016-02-03 09:31:15 -0500

    fixed merge conflicts in gift model

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,19 +1,15 @@
 class Gift < ActiveRecord::Base
   belongs_to :user
-  # has_many :requests
-
-  # has_many :requests, foreign_key: :wanted_id
-
-  # accepts_nested_attributes_for :requests
+  has_many :requests
+  has_many :requests, foreign_key: :wanted_id
+  accepts_nested_attributes_for :requests
   has_many :gift_categories, dependent: :destroy
   has_many :categories, through: :gift_categories, dependent: :destroy
   accepts_nested_attributes_for :categories
   validates_presence_of :title, :value, :description, :brand, :image
   mount_uploader :image, AssetUploader
-<<<<<<< HEAD
   include PgSearch
   pg_search_scope :search, against: [:title, :description, :brand]
-=======
 
   def requests_wanted
     Request.where(wanted_id: self.id)
@@ -23,5 +19,4 @@ class Gift < ActiveRecord::Base
     Request.where(unwanted_id: self.id)
   end
 
->>>>>>> master
 end
