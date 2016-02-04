ac05ed6fe270c6578c5bed2bcda56f4317a5180a
nat-ser <seryako2@tcnj.edu>
2016-02-04 17:53:40 -0500

    Revert "Swap functionality complete"

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,7 +1,5 @@
 class Gift < ActiveRecord::Base
   belongs_to :user
-  has_many :items, class_name: "Swap", foreign_key: :item_id
-  has_many :swapped_items, class_name: "Swap", foreign_key: :swapped_item_id
   has_many :requests, foreign_key: :wanted_id, dependent: :destroy
   accepts_nested_attributes_for :requests
   has_many :gift_categories, dependent: :destroy
@@ -20,14 +18,4 @@ class Gift < ActiveRecord::Base
   def requests_made
     Request.where(unwanted_id: id)
   end
-
-  def self.get_all_gifts
-    @gifts =  self.all.select do |gift|
-      if Swap.find_by(item_id: gift.id) == nil
-        gift
-      end
-    end
-    @gifts
-  end
-
 end
