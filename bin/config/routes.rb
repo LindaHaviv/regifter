ff9423c479d6f7991d529424a55a8a09735fed91
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2016-02-07 01:32:23 -0500

    search functionality

---
+++ bin/config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  root "home#index"
+
+  delete 'signout', to: "sessions#destroy"
+
+  get "signup", to: "users#new"
+
+  get "signin", to: "sessions#new"
+  post "signin", to: "sessions#create"
+  resources :users, :categories, :search, :gifts, :requests, :swaps
+
+  resources :gifts do
+    resources :requests
+  end
+
+  resources :conversations do
+    resources :messages
+  end
+  post 'gifts/accept/:id', to: 'gifts#accept', as: 'accept'
+end
